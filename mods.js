5d2bdc1889a7276637516c13e0086497dc630fb4
NopeScoper <122117554+NopeScoper@users.noreply.github.com>
2026-02-14 23:22:00 +0500

    Update mods.js

--- mods.js
+++ mods.js
@@ -5,5 +5,12 @@
     "url": "https://raw.githubusercontent.com/NopeScoper/arz-mods/main/scripts/moonloader.zip",
     "filename": "moonLoader.zip",
     "type": "zip"
+  },
+  {
+    "name": "Server Time",
+    "description": "Отображает время",
+    "url": "https://raw.githubusercontent.com/NopeScoper/arz-mods/main/scripts/Server%20Time%20by%20Kalashnikov.lua",
+    "filename": "Server Time by Kalashnikov.lua",
+    "type": "file"
   }
 ]
