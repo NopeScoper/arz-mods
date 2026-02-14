b9a6fe98260e48c0b998bbddaf89992563a11931
NopeScoper <122117554+NopeScoper@users.noreply.github.com>
2026-02-14 23:34:10 +0500

    Update mods.js

--- mods.js
+++ mods.js
@@ -1,6 +1,6 @@
 [
   {
-    "name": "MoonLoader 0.26",
+    "name": "MoonLoader",
     "description": "База для работы скриптов. Ставить в первую очередь!",
     "url": "https://raw.githubusercontent.com/NopeScoper/arz-mods/main/scripts/moonloader.zip",
     "filename": "moonLoader.zip",
