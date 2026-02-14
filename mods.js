34e60e9f8c9747345967a4e0ff01facc05c8babb
NopeScoper <122117554+NopeScoper@users.noreply.github.com>
2026-02-14 22:05:14 +0500

    Update mods.js

--- mods.js
+++ mods.js
@@ -5,19 +5,5 @@
     "url": "https://raw.githubusercontent.com/NopeScoper/arz-mods/main/scripts/moonloader.zip",
     "filename": "moonLoader.zip",
     "type": "zip"
-  },
-  {
-    "name": "Money Separator",
-    "description": "Разделяет деньги точками (1.234.567$)",
-    "url": "ВСТАВЬ_СЮДА_RAW_ССЫЛКУ_НА_MONEY_SEPARATOR.LUA",
-    "filename": "money_separator.lua",
-    "type": "file"
-  },
-  {
-    "name": "Fast Connect",
-    "description": "Быстрое подключение к серверу",
-    "url": "ВСТАВЬ_СЮДА_RAW_ССЫЛКУ_НА_FCONNECT.LUA",
-    "filename": "fconnect.lua",
-    "type": "file"
   }
 ]
